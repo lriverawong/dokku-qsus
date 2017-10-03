e02b88660b1b572924b38f3ee502ca55e505c2b7
luis <lriverawong@outlook.com>
2017-10-03 19:04:29 -0400

    added contact page with emails, routes, scss files

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'info#home'
 
-  # # INFO
-  # ['about_us', 'contact_us', 'our_team'].each do |page|
-  #   get page, controller: 'info', action: page
-  # end
+  # INFO
+  # ['about_us', 'contact_us', 'our_team']
+  ['contact_us'].each do |page|
+    get page, controller: 'info', action: page
+  end
 
 end
