a447e234a0cbf357d1ffd520da016d4855a396f0
Andrew-Bootsma <a_bootsma@outlook.com>
2017-10-04 01:20:34 -0400

    its a blog

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,3 +1,11 @@
 class InfoController < ApplicationController
 
+  def home
+    redirect_to articles_path if logged_in?
+  end
+
+  def contact_us
+
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'info#home'
 
+  resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :categories, except: [:destroy]
+
   # INFO
   # ['about_us', 'contact_us', 'our_team']
   ['contact_us'].each do |page|
