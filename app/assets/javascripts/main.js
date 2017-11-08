ee59ba1d35723dc13f9ca3898825c2ffb6d9909d
Luis <lriverawong@outlook.com>
2017-11-07 23:13:55 -0500

    re-added is-middle css class for nav

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,3 @@
-/*
-	Dimension by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
     console.log("Function started");
     skel.breakpoints({
@@ -16,7 +10,6 @@
     });
 
     $(function() {
-        console.log("Inside 2nd function?");
         var	$window = $(window),
             $body = $('body'),
             $wrapper = $('#wrapper'),
